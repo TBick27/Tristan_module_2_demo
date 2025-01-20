879c597563728f82387ade689e0c5f82aeabbd5b
Tristan Bickel <tbickel@academic.rrc.ca>
2025-01-20 11:50:50 -0600

    small changes

---
+++ Module2.py
@@ -0,0 +1,15 @@
+"""
+Mod 2 Demo
+Tristan Bickel
+Date: 2025-01-20
+"""
+# Single line comment
+"""multi line comment"""
+absolute_value = abs(-12)
+
+print('absolute value:', absolute_value)
+print('Absolute value is:',abs(-15))
+
+
+#Function embedded within another function. 
+# print('absolute value:', abs(-15))
